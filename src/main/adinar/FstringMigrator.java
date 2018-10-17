ce909ea1ac0b30115fc824fb0f28d9d85c81a106
Adrian Naruszko <naruszko.adrian@gmail.com>
2018-10-17 20:18:31 +0200

    Add support for numbered and non-named args

--- src/main/adinar/FstringMigrator.java
+++ src/main/adinar/FstringMigrator.java
@@ -16,35 +16,261 @@
 
 package main.adinar;
 
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.CommonDataKeys;
-import com.intellij.openapi.command.WriteCommandAction;
-import com.intellij.openapi.editor.Document;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.SelectionModel;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.TextRange;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class FstringMigrator extends AnAction {
-    @Override
-    public void actionPerformed(final AnActionEvent e) {
-        final Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
-        final Project project = e.getProject();
+import org.codehaus.plexus.util.StringUtils;
+import org.jetbrains.annotations.Nullable;
 
-        final Document document = editor.getDocument();
-        final SelectionModel selectionModel = editor.getSelectionModel();
+public class FstringMigrator {
+    private static final String FORMAT_1 = "\".format(";
+    private static final String FORMAT_2 = "'.format(";
+    String buffer;
+    StringBuffer result;
+    private FormatInfo firstFormatInfo;
+    private String firstFormat;
 
-        final int start = selectionModel.getSelectionStart();
-        final int end = selectionModel.getSelectionEnd();
+    public FstringMigrator(String selection) {
+        this.buffer = selection;
+        result = new StringBuffer();
+    }
 
-        WriteCommandAction.runWriteCommandAction(project, () -> {
-                    String selection = document.getText(new TextRange(start, end));
+    public String getResultReplace() {
+        while (!buffer.isEmpty()) {
+            firstFormat = getFirstFormat();
+            if (firstFormat == null) break;
+            String argumentsRaw = getArgumentsWithoutBrackets();
+            Arguments mapArguments = parseArguments(argumentsRaw);
+            applyArgumentsToFormat(mapArguments, firstFormat);
+        }
 
-                    document.replaceString(start, end, new MigratorUtils(selection).getResultReplace());
-                    selectionModel.removeSelection();
-                }
-        );
-        selectionModel.removeSelection();
+        return result.toString();
+    }
+
+    void applyArgumentsToFormat(Arguments arguments, String text) {
+        for (Map.Entry<String, String> entry : arguments.map.entrySet()) {
+            text = fillAllFormatsWithEntry(text, entry, this::textReplace);
+        }
+
+        Map<String, String> emptyHandler = new HashMap<>();
+
+        for (String arg : arguments.list) {
+            emptyHandler.put("", arg);
+            Map.Entry<String, String> entryElement = emptyHandler.entrySet().iterator().next();
+            text = fillAllFormatsWithEntry(text, entryElement, this::textReplaceFirst);
+        }
+
+        result.append(text);
+    }
+
+    private String textReplace(TextReplaceArgs args) {
+        return args.text.replace(args.template, args.data);
+    }
+
+    private String textReplaceFirst(TextReplaceArgs args) {
+        return StringUtils.replaceOnce(args.text, args.template, args.data);
+    }
+
+    private String fillAllFormatsWithEntry(String text, Map.Entry<String,String> entry,
+            Function<TextReplaceArgs, String> fun) {
+        text = fillWithEntry(text, "{%s.", entry, fun);
+        text = fillWithEntry(text, "{%s!", entry, fun);
+        return fillWithEntry(text, "{%s}", entry, fun);
+    }
+
+    private String fillWithEntry(String text, String format, Map.Entry<String, String> entry,
+            Function<TextReplaceArgs, String> fun) {
+        return fun.apply(new TextReplaceArgs(text, String.format(format, entry.getKey()),
+                String.format(format, entry.getValue())));
+    }
+
+    Arguments parseArguments(String argumentsRaw) {
+        Map<String, String> argumentsMap = new HashMap<>();
+        List<String> argumentsList = new ArrayList<>();
+        int cnt = 0, start = 0, end = 0;
+        List<String> list = new ArrayList<>();
+        for (char c : argumentsRaw.toCharArray()) {
+            if (c == '(') cnt++;
+            if (c == ')') cnt--;
+
+            if (c == ',' && cnt == 0) {
+                list.add(argumentsRaw.substring(start, end));
+                start = end + 1;
+            }
+
+            end++;
+        }
+
+        if (start != end - 1) {
+            list.add(argumentsRaw.substring(start));
+        }
+
+        for (String arg : list) {
+            String[] vals = arg.split("=");
+            switch (vals.length) {
+            case 1:
+                argumentsList.add(vals[0].trim());
+                break;
+            case 2:
+                argumentsMap.put(vals[0].trim(), vals[1].trim());
+                break;
+            default:
+                assert false;  // Seems like syntax error
+            }
+        }
+
+        Arguments result = new Arguments(argumentsMap, argumentsList);
+        result.addNumberedArgsToMapped();
+
+        return result;
+    }
+
+    private String adjustValIfStringType(String val) {
+        String res = replaceQuoteTypeIfNeededAndPossible(val, '\'', '"');
+        if (res == null) {
+            res = replaceQuoteTypeIfNeededAndPossible(val, '"', '\'');
+
+            if (res == null) {
+                String valWithoutQuotes = val.substring(1, val.length() - 1);
+                return String.format("\\%c%s\\%c",
+                        firstFormatInfo.quoteType, valWithoutQuotes,
+                        firstFormatInfo.quoteType);
+            }
+        }
+
+        return res;
+    }
+
+    @Nullable
+    private String replaceQuoteTypeIfNeededAndPossible(String val, char currentChar, char wantedChar) {
+        char[] cv = val.toCharArray();
+        if (cv[0] == currentChar && firstFormatInfo.quoteType == currentChar) {
+            if (!val.contains(String.valueOf(wantedChar))) {
+                cv[0] = wantedChar;
+                cv[cv.length - 1] = wantedChar;
+            } else {
+                return null;
+            }
+        }
+
+        return new String(cv);
+    }
+
+    String getFirstFormat() {
+        firstFormatInfo = getFirstFormatInfo();
+        if (firstFormatInfo == null || firstFormatInfo.startPos < 0) {
+            result.append(buffer);
+            return null; // No data left
+        }
+        result.append(buffer, 0, firstFormatInfo.startPos);
+        result.append("f");
+        String format = String.format("%c%s%c",
+                firstFormatInfo.quoteType, buffer.substring(firstFormatInfo.getFormatStartWithoutQuotePos(),
+                        firstFormatInfo.endPos), firstFormatInfo.quoteType);
+        buffer = buffer.substring(firstFormatInfo.getAfterFormatWordPos());
+        return format;
+    }
+
+    private String getArguments() {
+        int pos = 0, cnt = 0;
+        boolean wasOpened = false;
+
+        while (cnt > 0 || !wasOpened) {
+            if (buffer.charAt(pos) == '(') {
+                cnt++;
+                wasOpened = true;
+            }
+            if (buffer.charAt(pos) == ')') cnt--;
+            pos++;
+        }
+
+        String result = buffer.substring(0, pos);
+        buffer = buffer.substring(pos);
+        return result;
+    }
+
+    String getArgumentsWithoutBrackets() {
+        String str = getArguments();
+        return str.substring(1, str.length() - 1);
+    }
+
+    private FormatInfo getFirstFormatInfo() {
+        char quoteType = '"';
+        int formatEnd = buffer.indexOf(FORMAT_1);
+        int formatEnd2 = buffer.indexOf(FORMAT_2);
+        if (formatEnd < 0 || (formatEnd2 >= 0 && formatEnd > formatEnd2)) {
+            formatEnd = buffer.indexOf(FORMAT_2);
+            quoteType = '\'';
+        }
+        int lastCharBeforeClosingQuotePos = formatEnd - 1;
+
+        if (lastCharBeforeClosingQuotePos < 0) return null;
+
+        String prefixWithFormat = buffer.substring(0, lastCharBeforeClosingQuotePos);
+        Pattern openingQuotePattern = Pattern.compile(".*(?<!\\\\)" + quoteType);
+        Matcher matcher = openingQuotePattern.matcher(prefixWithFormat);
+        int formatStart = 0;
+        if (matcher.find()) {
+            formatStart = matcher.group(0).length() - 1;
+        }
+
+        return new FormatInfo(formatStart, formatEnd, quoteType);
+    }
+
+    private class FormatInfo {
+        final int startPos;
+
+        public FormatInfo(int startPos, int endPos, char quoteType) {
+            this.startPos = startPos;
+            this.endPos = endPos;
+            this.quoteType = quoteType;
+        }
+
+        final int endPos;
+        final char quoteType;
+
+        public int getFormatStartWithoutQuotePos() {
+            return startPos + 1;
+        }
+
+        public int getAfterFormatWordPos() {
+            assert FORMAT_1.length() == FORMAT_2.length();
+            return endPos + FORMAT_1.length() - 1; // Minus one so the closing bracket will be on the right
+        }
+    }
+
+    class Arguments {
+        final Map<String, String> map;
+        final List<String> list;
+
+        private Arguments(Map<String, String> map, List<String> list) {
+            this.map = map;
+            this.list = list;
+        }
+
+        // Handles cases like '{0} {1}'.format(first, second)
+        public void addNumberedArgsToMapped() {
+            for (int i=0; i<list.size(); i++) {
+                map.put(String.valueOf(i), list.get(i));
+            }
+        }
+    }
+
+    class TextReplaceArgs {
+        final String text;
+        final String template;
+
+        public TextReplaceArgs(String text, String template, String data) {
+            this.text = text;
+            this.template = template;
+            this.data = data;
+        }
+
+        final String data;
     }
 }
---
+++ test/main/adinar/FstringMigratorTest.java
@@ -0,0 +1,87 @@
+package main.adinar;
+
+import java.util.Map;
+
+import main.adinar.FstringMigrator.Arguments;
+import junit.framework.TestCase;
+
+public class FstringMigratorTest extends TestCase {
+    public void testSingleIteration() {
+        FstringMigrator mu = new FstringMigrator(
+                "costam abcdef 'abe {aa} {bb}'.format(aa=cc, bb=ewq) abcdefgh"
+        );
+
+        String format = mu.getFirstFormat();
+        
+        assertEquals("costam abcdef f", mu.result.toString());
+        assertEquals("'abe {aa} {bb}'", format);
+        assertEquals("(aa=cc, bb=ewq) abcdefgh", mu.buffer);
+
+        String arguments = mu.getArgumentsWithoutBrackets();
+
+        assertEquals("aa=cc, bb=ewq", arguments);
+        assertEquals(" abcdefgh", mu.buffer);
+
+        Arguments args = mu.parseArguments(arguments);
+        Map<String, String> argumentsMap = args.map;
+
+        assertEquals(" abcdefgh", mu.buffer);
+        assertEquals("cc", argumentsMap.get("aa"));
+        assertEquals("ewq", argumentsMap.get("bb"));
+
+
+        mu.applyArgumentsToFormat(args, format);
+
+        assertEquals("costam abcdef f'abe {cc} {ewq}'", mu.result.toString());
+    }
+
+    public void testBasic() {
+        tst("costam abcdef 'abe {aa} {bb}'.format(aa=cc, bb=ewq) abcdefgh",
+                "costam abcdef f'abe {cc} {ewq}' abcdefgh");
+    }
+
+    public void testMultipleFormats() {
+        tst("costam abcdef 'abe {aa} {bb}'.format(aa=cc, bb=ewq) abcdefgh"
+                        + "aaaa \"{ee} {ff}\".format(ee=ciastko, ff=blabla)",
+                "costam abcdef f'abe {cc} {ewq}' abcdefghaaaa f\"{ciastko} {blabla}\"");
+    }
+
+    public void testMultipleFormats2() {
+        tst("costam abcdef \"abe {aa} {bb}\".format(aa=cc, bb=ewq) abcdefgh"
+                        + "aaaa '{ee} {ff}'.format(ee=ciastko, ff=blabla)",
+                "costam abcdef f\"abe {cc} {ewq}\" abcdefghaaaa f'{ciastko} {blabla}'");
+    }
+
+
+    public void testComplex() {
+        tst("now print some complex stuff 'abe {self.ciastko} {other.run()}'"
+                + ".format(self=slf, other=fun()) abcdefgh",
+                "now print some complex stuff f'abe {slf.ciastko} {fun().run()}' abcdefgh");
+    }
+
+    public void testFunCall() {
+        tst("'Would have send {msg}'.format(msg=msg.as_string())", "f'Would have send {msg.as_string()}'");
+    }
+
+    public void testQuotes() {
+        tst("sth 'Sending \\'{subject}\\' to {email}'.format(subject=subject, email=email_dest)",
+                "sth f'Sending \\'{subject}\\' to {email_dest}'");
+    }
+    public void testQuotes2() {
+        tst("'some text' sth 'Sending \\'{subject}\\' to {email}'.format(subject=subject, email=email_dest)",
+                "'some text' sth f'Sending \\'{subject}\\' to {email_dest}'");
+    }
+
+    public void testNumbered() {
+        tst("'{0} {1} {0}'.format(abc, def)", "f'{abc} {def} {abc}'");
+    }
+
+    public void testNonNamed() {
+        tst("'{} {} {}'.format(abc, def, ghi)", "f'{abc} {def} {ghi}'");
+    }
+
+    private void tst(String in, String out) {
+        FstringMigrator mu = new FstringMigrator(in);
+        assertEquals(out, mu.getResultReplace());
+    }
+}
